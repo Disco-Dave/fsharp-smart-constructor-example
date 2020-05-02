954f60309f8ac46d27e557c53648dcdb8b963cfb
David Burkett <david@davidburkett.xyz>
2020-05-02 12:17:35 -0400

    Upgraded to dotnet core 3.1 and removed the class definition

--- SmartConstructorExample.Tests/RangeTests.fs
+++ SmartConstructorExample.Tests/RangeTests.fs
@@ -5,10 +5,10 @@ open FsCheck.Xunit
 open SmartConstructorExample
 
 let private boundsAreValid (lowerBound : Bound<int>) (upperBound : Bound<int>) =
-    Bound.getValue lowerBound <= Bound.getValue upperBound
+    Bound.value lowerBound <= Bound.value upperBound
     
 let private boundsAreInValid (lowerBound : Bound<int>) (upperBound : Bound<int>) =
-    Bound.getValue lowerBound > Bound.getValue upperBound
+    Bound.value lowerBound > Bound.value upperBound
     
 let private valuesBelowLowerBound (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
    boundsAreValid lowerBound upperBound &&
@@ -27,34 +27,34 @@ let private valuesWithinRange (lowerBound : Bound<int>) (upperBound : Bound<int>
 let private valuesAboveUpperBound (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
    boundsAreValid lowerBound upperBound &&
        match upperBound with
-       | Exclusive bound -> value >= bound && Bound.getValue lowerBound <> bound
-       | Inclusive bound -> value > bound && Bound.getValue lowerBound <> bound
+       | Exclusive bound -> value >= bound && Bound.value lowerBound <> bound
+       | Inclusive bound -> value > bound && Bound.value lowerBound <> bound
        
 let private valuesNotWithinRange (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
     valuesBelowLowerBound lowerBound upperBound value || valuesAboveUpperBound lowerBound upperBound value
        
 [<Property>]
-let ``getValue always returns the correct value for Inclusive`` (value : int) =
+let ``value always returns the correct value for Inclusive`` (value : int) =
     let bound = Inclusive value
-    Bound.getValue bound = value
+    Bound.value bound = value
     
 [<Property>]
-let ``getValue always returns the correct value for Exclusive`` (value : int) =
+let ``value always returns the correct value for Exclusive`` (value : int) =
     let bound = Exclusive value
-    Bound.getValue bound = value
+    Bound.value bound = value
     
 [<Property>]
 let ``Only allowed to make valid ranges`` (lowerBound : Bound<int>) (upperBound : Bound<int>) =
-   boundsAreValid lowerBound upperBound ==> (Range.Make lowerBound upperBound).IsSome
+   boundsAreValid lowerBound upperBound ==> (Range.make lowerBound upperBound).IsSome
         
 [<Property>]
 let ``Does not allow invalid ranges`` (lowerBound : Bound<int>) (upperBound : Bound<int>) =
-    boundsAreInValid lowerBound upperBound ==> (Range.Make lowerBound upperBound).IsNone
+    boundsAreInValid lowerBound upperBound ==> (Range.make lowerBound upperBound).IsNone
 
 [<Property>]
 let ``Identifies values that are below the lower bound``(lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
     valuesBelowLowerBound lowerBound upperBound value ==>
-        match Range.Make lowerBound upperBound with
+        match Range.make lowerBound upperBound with
         | None -> false
         | Some range ->
             match Range.test range value with
@@ -64,7 +64,7 @@ let ``Identifies values that are below the lower bound``(lowerBound : Bound<int>
 [<Property>]
 let ``Identifies values that are within range`` (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
     valuesWithinRange lowerBound upperBound value ==>
-        match Range.Make lowerBound upperBound with
+        match Range.make lowerBound upperBound with
         | None -> false
         | Some range ->
             match Range.test range value with
@@ -74,7 +74,7 @@ let ``Identifies values that are within range`` (lowerBound : Bound<int>) (upper
 [<Property>]
 let ``Identifies values that are above the upper bound`` (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
     valuesAboveUpperBound lowerBound upperBound value ==>
-        match Range.Make lowerBound upperBound with
+        match Range.make lowerBound upperBound with
         | None -> false
         | Some range ->
             match Range.test range value with
@@ -84,13 +84,13 @@ let ``Identifies values that are above the upper bound`` (lowerBound : Bound<int
 [<Property>]
 let ``Returns true if value is within range`` (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
     valuesWithinRange lowerBound upperBound value ==>
-        match Range.Make lowerBound upperBound with
+        match Range.make lowerBound upperBound with
         | None -> false
         | Some range -> Range.isWithin range value
         
 [<Property>]
 let ``Returns false if value is not within range`` (lowerBound : Bound<int>) (upperBound : Bound<int>) (value : int) =
     valuesNotWithinRange lowerBound upperBound value ==>
-        match Range.Make lowerBound upperBound with
+        match Range.make lowerBound upperBound with
         | None -> false
         | Some range -> not <| Range.isWithin range value 
--- SmartConstructorExample/Range.fs
+++ SmartConstructorExample/Range.fs
@@ -6,48 +6,49 @@ type Bound<'a> =
 
 [<RequireQualifiedAccess>]
 module Bound =
-    let getValue bound =
-        match bound with
+    let value = function
         | Inclusive a -> a
         | Exclusive a -> a
 
-type Range<'a when 'a : comparison> private (lowerBound : Bound<'a>, upperBound : Bound<'a>) =
-    member val LowerBound = lowerBound
-    member val UpperBound = upperBound
-    
-    static member Make lowerBound upperBound =
-        let lowerValue = Bound.getValue lowerBound
-        let upperValue = Bound.getValue upperBound
 
-        if lowerValue <= upperValue then
-            Some <| new Range<'a>(lowerBound, upperBound)
-        else
-            None
+type Range<'a when 'a: comparison> =
+    private { LowerBound: Bound<'a>
+              UpperBound: Bound<'a> }
 
 type RangeResult =
     | BelowLowerBound
     | WithinBounds
     | AboveUpperBound
-    
+
 [<RequireQualifiedAccess>]
 module Range =
-    let private isWithinUpper<'a when 'a : comparison> (range : Range<'a>) value =
-        match range.UpperBound with
-        | Inclusive u -> value <= u
-        | Exclusive u -> value < u
+    let make lowerBound upperBound =
+        let lowerValue = Bound.value lowerBound
+        let upperValue = Bound.value upperBound
 
-    let private isWithinLower<'a when 'a : comparison> (range : Range<'a>) value =
-        match range.LowerBound with
-        | Inclusive l -> value >= l
-        | Exclusive l -> value > l
+        if lowerValue <= upperValue then
+            Some <| { LowerBound = lowerBound
+                      UpperBound = upperBound }
+        else
+            None
 
     let test range value =
-        match (isWithinLower range value, isWithinUpper range value) with
+        let isWithinUpperBound =
+            match range.UpperBound with
+            | Inclusive u -> value <= u
+            | Exclusive u -> value < u
+
+        let isWithinLowerBound =
+            match range.LowerBound with
+            | Inclusive l -> value >= l
+            | Exclusive l -> value > l
+
+        match (isWithinLowerBound, isWithinUpperBound) with
         | (true, true) -> WithinBounds
         | (false, _) -> BelowLowerBound
         | (_, false) -> AboveUpperBound
 
     let isWithin range value =
-        match (test range value) with
+        match test range value with
         | WithinBounds -> true
         | _ -> false
